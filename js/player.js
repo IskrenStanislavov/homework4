09537ab1101635f6fc1e9d8900afa34ca5244aa7
Isko <iskren.jgd@gmail.com>
2015-05-02 12:57:51 +0300

    add player.id

--- js/player.js
+++ js/player.js
@@ -2,8 +2,13 @@ define(function (require) {
 	var PIXI = require('libs/pixi.dev');
 	var Bangup = require('bangup');
 
-	var Player = function(initialAmount){
+	var Player = function(initialAmount, id){
 		PIXI.DisplayObjectContainer.call(this);
+		if (id === undefined){
+			this.id = "";
+		} else {
+			this.id = id;
+		}
 
 		this.balance = this.addChild(new Balance(1000));
 		this.bet     = this.addChild(new Bet());
@@ -11,7 +16,7 @@ define(function (require) {
 		this.amount = initialAmount;
 		this.update( this.amount, this.amount );
 		this.setAmountTextXY(140, 18);
-		this.label = this.addChild(new PIXI.Text("Player:", { font: 'bold 24px Arial', fill: '#f3d601', align: 'left' }));
+		this.label = this.addChild(new PIXI.Text("Player:" + this.id, { font: 'bold 24px Arial', fill: '#f3d601', align: 'left' }));
 	};
 
 	Player.prototype = Object.create( PIXI.DisplayObjectContainer.prototype );
