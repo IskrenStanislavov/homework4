793ee1e395dce6df0c3349e559c6fa9bea4bfbcc
Isko <iskren.jgd@gmail.com>
2015-05-02 12:56:20 +0300

    add Array.prototype.range(start,end)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
+Array.prototype.range = function(start,end){
+	return Array.apply(null, Array(end-start+1)).map(function (_, i){
+	    // http://stackoverflow.com/a/10050831/3345926
+	    return start+i;
+	});
+};
+
+
 require(["libs/pixi.dev", "libs/TweenMax.min", "settings", "stage", "game" ],
 	function( PIXI, TweenMax, settings, stage, Game ) {
 	
